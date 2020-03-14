f1019e747a7de10444befdf2b5d135819306bb0e
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-03-14 18:28:23 +0530

    ADDED: Code and Test to check if the object is null or not

--- src/main/java/com/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurement.java
@@ -1,9 +1,20 @@
 package com.quantitymeasurement;
 
+import java.util.Objects;
+
 public class QuantityMeasurement {
     public boolean calculatingMeasurements(int paramOne, int paramTwo) {
         if (paramOne == paramTwo)
             return true;
         return false;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!(object instanceof QuantityMeasurement))
+            return false;
+        return true;
     }
 }
--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Objects;
+
 public class QuantityMeasurementTest {
     private QuantityMeasurement object;
 
@@ -12,9 +14,16 @@ public class QuantityMeasurementTest {
         object = new QuantityMeasurement();
     }
 
+
     @Test
     public void givenTwoUnit_IfEqual_ShouldReturnTrue() {
         boolean result = object.calculatingMeasurements(0, 0);
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenObject_IfNotNull_ShouldReturnFalse() {
+        boolean result = object.equals(null);
+        Assert.assertFalse(result);
     }
 }
